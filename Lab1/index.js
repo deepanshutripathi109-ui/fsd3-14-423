f910ff61fc662682f464e8b847d54e42875c293d
Deepanshu Tripathi <deepanshutripathi109@gmail.com>
2026-07-17 15:41:41 +0530

    lab 1 started

---
+++ Lab1/index.js
@@ -0,0 +1,14 @@
+/* Write a function to take your name and print it with hello msg */
+function sayHello(name){
+    console.log(`hello ${name}`);
+}
+
+const sayHi=(name)=>{
+    console.log("Hi",name);
+}
+sayHello("Deepanshu Tripathi");
+sayHi("Dev singh");
+
+/*Take two argumnets and return their sum*/
+const twoSum =(a,b)=>a+b;
+console.log(twoSum(5,3));
