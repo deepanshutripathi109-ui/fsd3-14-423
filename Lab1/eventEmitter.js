c61bb0550d24ff8522e6be5e7f8d2cbb6d662eae
Deepanshu Tripathi <deepanshutripathi109@gmail.com>
2026-07-21 10:07:13 +0530

    add events2

--- Lab1/eventEmitter.js
+++ Lab1/eventEmitter.js
@@ -1,25 +1,32 @@
+import{log} from "node:console";
 import {EventEmitter} from "node:events";
 const task = new EventEmitter();
 const sayHi = (name) =>{
     console.log(`Loggedin ${name}`);
 }; 
-task.on("greet",()=>{
-    console.log("Logged Out");
-});
+// task.on("greet",()=>{
+//     console.log("Logged Out");
+// });
+task.on("greet",sayHi);
 task.once("greet",()=>{
     console.log("system started...");
 });
 
-task.on("greet",sayHi)
 
-task.on("greet",(name)=>{
-    console.log(`system is shutting down by ${name}`)
 
+task.on("exit",sayHi);
+
+
+task.once("exit",(name)=>{
+    console.log(`system is shut down by ${name}`);
 })
 
 
 
+task.on("greet",sayHi);
 task.emit('greet','Deepanshu Tripathi')
+task.off("greet",sayHi);
 task.emit("greet","Sonia singh");
-task.emit("greet","Rahul Singh")
-task.off("exit","Manager");
+// task.emit("greet","Rahul Singh");
+task.emit("exit","Manager");
+
