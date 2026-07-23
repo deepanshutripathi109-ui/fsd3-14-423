c015515a6f8139f68c061cc2438198e6e982a217
Deepanshu Tripathi <deepanshutripathi109@gmail.com>
2026-07-23 15:34:37 +0530

    task1 modified

--- Lab1/EventLoop/task1.js
+++ Lab1/EventLoop/task1.js
@@ -1,18 +1,33 @@
 const f1=()=>{
-    console.log("f1");
+    console.log("f1 start");
+    f2();
+    console.log("f1 running");
+    console.log("f1 end");
 }
 const f2=()=>{
-    console.log("f2");
+    console.log("f2 start");
+    f3();
+    console.log("f2 running");
+    console.log("f2 end");
 }
 const f3=()=>{
-    console.log("f3");
+    console.log("f3 starts");
+    console.log("f3 running");
+    console.log("f3 end");
 }
 
 function main(){
-    console.log("main");
+    console.log("main starts");
     f1();
-    f2();
-    f3();
-    console.log("end main");
+
+    console.log("main ends");
 }
-main();
+main();     //Synchronous call
+
+// in asynchronous we use event loop to manage call stack
+
+/*asynchronous calls  using timers : */
+//  1)set timeout
+//  2)set immediate
+//  3)process.nextTick
+// 4)set Interval   
