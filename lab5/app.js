a0a7132563d6bd1b6bd158e9dd162e2e03422f8e
Deepanshu Tripathi <deepanshutripathi109@gmail.com>
2026-09-17 14:00:49 +0530

    basic routing using express

--- lab5/app.js
+++ lab5/app.js
@@ -1,11 +1,29 @@
 import express from 'express'
 
-const app =express();
-app.get("/about",(req,res)=>{
-    res.send("<h1>Hello Express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1> Hello Express");
 });
-app.use("*",(req,res)=>{
-    res.send("route not found");
+
+app.get('/about',(req,res)=>{
+    res.send("We are FSD Developer")
 })
 
-app.listen(3000,console.log("Server is running"));
+app.post('/login',(req,res)=>{
+    res.send({msg:'user login'})
+})
+
+app.put('/user/update/1',(req,res)=>{
+    res.send({msg:'user update'})
+})
+
+app.delete('/users/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+
+app.use((req,res)=>{
+    res.status(404).send("Not found")
+})
+
+app.listen(3333, ()=> console.log("Server is running at 3333"))
